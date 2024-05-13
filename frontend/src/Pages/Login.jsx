c7d554e9102febc2b5cfdbdd455086f138ff00de
utsavcoder77 <baralutsav89@gmail.com>
2024-05-13 21:02:04 +1000

    validate form

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,11 +1,135 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../Components/Logo";
 
-function Login() {
+export default function Register() {
   return (
     <>
-      <h2>Login</h2>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-white">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex min-h-full flex-1">
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <Logo width="150px" />
+              <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Register using your personal details
+              </h2>
+              <p className="mt-2 text-sm leading-6 text-gray-500">
+                Not a member yet?{" "}
+                <Link
+                  to="/register"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Register here
+                </Link>
+              </p>
+            </div>
+
+            <div className="mt-10">
+              <div>
+                <form action="#" method="POST" className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email address
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <input
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                      <label
+                        htmlFor="remember-me"
+                        className="ml-3 block text-sm leading-6 text-gray-700"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+
+                    <div className="text-sm leading-6">
+                      <Link
+                        to="/reset-password"
+                        className="font-semibold text-indigo-600 hover:text-indigo-500"
+                      >
+                        Forgot password?
+                      </Link>
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Sign in
+                    </button>
+                  </div>
+                </form>
+              </div>
+
+              <div className="mt-10">
+                <div className="relative">
+                  <div
+                    className="absolute inset-0 flex items-center"
+                    aria-hidden="true"
+                  >
+                    <div className="w-full border-t border-gray-200" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1528911767216-351e4afc2a93?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8emFyYSUyMHByb2R1Y3R8ZW58MHx8MHx8fDA%3D"
+            alt=""
+          />
+        </div>
+      </div>
     </>
   );
 }
-
-export default Login;
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,13 +1,163 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-function Register() {
+import Logo from "../Components/Logo";
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  function submitData(data) {
+    console.log(data);
+  }
+
   return (
     <>
-      <div>
-        <h2>Register</h2>
+      <div className="flex min-h-full flex-1">
+        <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <Logo width="150px" />
+              <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Register using your personal details
+              </h2>
+              <p className="mt-2 text-sm leading-6 text-gray-500">
+                Already a member?{" "}
+                <Link
+                  to="/login"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Login to your account
+                </Link>
+              </p>
+            </div>
+
+            <div className="mt-10">
+              <div></div>
+              <div>
+                <form onSubmit={handleSubmit(submitData)} className="space-y-6">
+                  <div className="flex gap-10">
+                    <div className="w-full">
+                      <label
+                        htmlFor="firstName"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        First name*
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="firstName"
+                          {...register("firstName")}
+                          type="text"
+                          autoComplete="firstName"
+                          className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full">
+                      <label
+                        htmlFor="lastName"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Last name*
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          id="lastName"
+                          {...register("lastName")}
+                          type="text"
+                          autoComplete="lastName"
+                          className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="mobile"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Mobile
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="mobile"
+                        {...register("mobile")}
+                        type="text"
+                        autoComplete="mobile"
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email*
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="email"
+                        {...register("email")}
+                        type="email"
+                        autoComplete="email"
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password*
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="password"
+                        {...register("password")}
+                        type="password"
+                        autoComplete="current-password"
+                        className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Register
+                    </button>
+                  </div>
+                  <div className="flex items-center justify-between text-sm leading-6">
+                    <Link
+                      to="/register"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                      Back to Home
+                    </Link>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative hidden w-0 flex-1 lg:block">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1691096671143-d05c501339bc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGVjb21tZXJjZSUyMHByb2R1Y3R8ZW58MHx8MHx8fDA%3D"
+            alt=""
+          />
+        </div>
       </div>
     </>
   );
 }
-
-export default Register;
